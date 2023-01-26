42a16214f38b76b2e2f7bc2d02eb5e71f30a3631
Boloo66 <oguntadeemmanuel66@icloud.com>
2023-01-26 07:08:06 +0100

    UTF-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" This module validates a UTF-8 entry"""
+
+from typing import List
+
+
+def count_bit(num: int) -> int:
+    """ Counts the number of starting 1's in a byte and
+    return the count """
+
+    count: int = 0
+    for i in range(7, -1, -1):
+        if(num & (1 << i)):
+            count += 1
+        else:
+            return count
+
+    return count
+
+
+def validUTF8(data: List[int]) -> bool:
+    """ Validates a utf-8 data and return True
+    , else False"""
+
+    counter: int = 0
+    for d in data:
+        if not counter:
+            counter = count_bit(d)
+
+            if counter == 0:
+                continue
+            elif counter == 1 or counter > 4:
+                return False
+            else:
+                counter -= 1
+        else:
+            if(count_bit(d) != 1):
+                return False
+            counter -= 1
+
+    return counter == 0
